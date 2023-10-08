0cf5042a4fe3d4f53fd6d5772078bade57a6b48e
khaled39ab <khaled39ab@gmail.com>
2023-10-08 17:45:17 +0600

    new section added

---
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -0,0 +1,17 @@
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+
+const PopularMenu = () => {
+    return (
+        <div>
+            <SectionTitle
+                heading={'From Our Menu'}
+                subheading={'Popular Menu'}
+            ></SectionTitle>
+            <div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default PopularMenu;
