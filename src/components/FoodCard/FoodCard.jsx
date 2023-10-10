58bb36a515d5f6f32b59af324be3b86f5dc54142
khaled39ab <khaled39ab@gmail.com>
2023-10-10 23:59:37 +0600

    order page done

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,14 +1,13 @@
 
-const FoodCard = ({ foodImg, foodName, item }) => {
+const FoodCard = ({ item }) => {
     const { name, image, recipe, price } = item;
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
-            <figure><img src={foodImg || image} alt={`Caesar ${foodName || name}`} className="rounded-xl" /></figure>
-            {
-                price && <p>${price}</p>
-            }
+            <figure><img src={image} alt={`Caesar ${name}`} className="rounded-xl" /></figure>
+            <p className="absolute right-0 mt-5 mr-6 bg-black text-white px-3">${price}</p>
+
             <div className="card-body items-center text-center">
-                <h2 className="card-title font-bold">Caesar {foodName || name}</h2>
+                <h2 className="card-title font-bold">Caesar {name}</h2>
                 <p>{recipe ? recipe : 'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.'}</p>
                 <div className="card-actions">
                     <button className="btn  btn-outline border-b-4 uppercase">Add to Cart</button>
