950982fab512425b494731798f1d742f04bf0e57
khaled39ab <khaled39ab@gmail.com>
2023-10-10 23:57:04 +0600

    restore code

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -4,6 +4,9 @@ const FoodCard = ({ foodImg, foodName, item }) => {
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
             <figure><img src={foodImg || image} alt={`Caesar ${foodName || name}`} className="rounded-xl" /></figure>
+            {
+                price && <p>${price}</p>
+            }
             <div className="card-body items-center text-center">
                 <h2 className="card-title font-bold">Caesar {foodName || name}</h2>
                 <p>{recipe ? recipe : 'Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.'}</p>
--- src/pages/Home/ChefRecommends/ChefRecommends.jsx
+++ src/pages/Home/ChefRecommends/ChefRecommends.jsx
@@ -1,7 +1,6 @@
 import salad from '../../../assets/home/slide1.jpg';
 import pizza from '../../../assets/home/slide2.jpg';
 import soup from '../../../assets/home/slide3.jpg';
-import FoodCard from '../../../components/FoodCard/FoodCard';
 import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 
 const ChefRecommends = () => {
@@ -14,9 +13,44 @@ const ChefRecommends = () => {
 
             <div className='grid md:grid-cols-3 gap-8 mt-10 pl-3'>
 
-                <FoodCard foodImg={salad} foodName={'Salad'}></FoodCard>
-                <FoodCard foodImg={pizza} foodName={'Pizza'}></FoodCard>
-                <FoodCard foodImg={soup} foodName={'Soup'}></FoodCard>
+                <div className="card w-96 bg-base-100 shadow-xl">
+                    <figure>
+                        <img src={salad} alt="Caeser Salad" className="rounded-xl" />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title font-bold">Caeser Salad</h2>
+                        <p>Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.</p>
+                        <div className="card-actions">
+                            <button className="btn  btn-outline border-b-4 uppercase">Add to Cart</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="card w-96 bg-base-100 shadow-xl">
+                    <figure>
+                        <img src={pizza} alt="Caeser Pizza" className="rounded-xl" />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title font-bold">Caeser Pizza</h2>
+                        <p>Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.</p>
+                        <div className="card-actions">
+                            <button className="btn  btn-outline border-b-4 uppercase">Add to Cart</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="card w-96 bg-base-100 shadow-xl">
+                    <figure>
+                        <img src={soup} alt="Caeser Soup" className="rounded-xl" />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title font-bold">Caeser Soup</h2>
+                        <p>Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.</p>
+                        <div className="card-actions">
+                            <button className="btn  btn-outline border-b-4 uppercase">Add to Cart</button>
+                        </div>
+                    </div>
+                </div>
 
             </div>
         </div>
